46365cf8a1db01e0c2e6b618bfa06b5c21791286
Awaish Ehsan <awaishehsan077@gmail.com>
2026-03-28 03:00:20 +0530

    add resend verification email and refresh access token

--- src/controllers/auth.controllers.js
+++ src/controllers/auth.controllers.js
@@ -3,7 +3,8 @@ import { ApiResponse } from '../utils/api-response.js'
 import { ApiError } from '../utils/api-error.js'
 import { asyncHandler } from '../utils/async-handler.js'
 import { emailVerificationMailgenContent, sendEmail } from '../utils/mail.js'
-
+import crypto from 'crypto'
+import jwt from "jsonwebtoken"
 
 const generateAccessAndRefreshTokens = async (userId) => {
     try {
@@ -15,7 +16,6 @@ const generateAccessAndRefreshTokens = async (userId) => {
         await user.save({ validateBeforeSave: false })
         return { accessToken, refreshToken }
     } catch (error) {
-        console.log("ACTUAL ERROR:", error)
         throw new ApiError(500, "Something went wrong while generating tokens", [])
     }
 }
@@ -143,4 +143,95 @@ const getCurrentUser = asyncHandler(async(req,res)=>{
     )
 })
 
-export { registerUser, login ,logoutUser , getCurrentUser}
+const verifyEmail = asyncHandler(async(req,res)=>{
+    const {verificationToken} = req.params
+
+    if(!verificationToken){
+        throw new ApiError(400,"Email verification token is missing")
+    }
+
+    let hashedToken = crypto.createHash("sha256").update(verificationToken).digest("hex")
+
+    const user = await User.findOne({
+        emailVerificationToken : hashedToken,
+        emailVerificationExpiry: {$gt:Date.now()}
+    })
+
+    if(!user){
+        throw new ApiError(400,"Token is invalid or expired")
+    }
+
+    user.emailVerificationToken=undefined
+    user.emailVerificationExpiry=undefined
+
+    user.isEmailVerified = true
+
+    await user.save({validateBeforeSave:false})
+
+    return res.status(200).json(new ApiResponse(200,{isEmailVerified : true},"Email is verified"))
+})
+
+const resendEmailVerification = asyncHandler(async(req,res)=>{
+    const user = await User.findById(req.user?._id);
+
+    if(!user){
+        throw new ApiError(404,"User does not exist")
+    }
+
+    if(user.isEmailVerified){
+        throw new ApiError(409,"Email is already verified")
+    }
+
+    const { unHashedToken, hashedToken, tokenExpiry } = user.generateTemporaryToken()
+    user.emailVerificationToken = hashedToken
+    user.emailVerificationExpiry = tokenExpiry
+    await user.save({ validateBeforeSave: false })
+
+    await sendEmail({
+        email: user?.email,
+        subject: "Email Verification",
+        mailgenContent: emailVerificationMailgenContent(
+            user.username,
+            `${req.protocol}://${req.get('host')}/api/v1/users/verify-email/${unHashedToken}`
+        ),
+    })
+
+    return res.status(200).json(new ApiResponse(200,{},"Mail has been sent to your Email Id"))
+})
+
+const refreshAccessToken = asyncHandler(async(req,res)=>{
+
+    const incomingRefreshToken = req.cookies.refreshToken || req.body.refreshToken
+    if(!incomingRefreshToken){
+        throw new ApiError(401,"Unauthorized access")
+    }
+
+    try{
+        const decodedToken = jwt.verify(incomingRefreshToken,process.env.REFRESH_TOKEN_SECRET)
+        const user = await User.findById(decodedToken?._id)
+
+        if(!user){
+            throw new ApiError(401,"Invalid refresh token")
+        }
+
+        if(incomingRefreshToken !== user?.refreshToken){
+            throw new ApiError(401,"Refresh Token is Expired")
+        }
+
+        const options = {
+            httpOnly:true,
+            secure:true
+        }
+
+        const {accessToken,refreshToken:newRefreshToken} = await generateAccessAndRefreshTokens(user._id)
+        
+
+        return res.status(200).cookie("accessToken",accessToken,options).cookie("refreshToken",newRefreshToken,options).json(new ApiResponse(200,{accessToken,refreshToken:newRefreshToken},
+            "Access token refreshed"
+        ))
+    }catch(error){
+        throw new ApiError(401,"Invalid Refresh Token")
+    }
+})
+
+export { registerUser, login ,logoutUser , getCurrentUser,verifyEmail,resendEmailVerification,refreshAccessToken}
