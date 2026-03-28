33da7b861b1d041822732d26917c132e93540310
Awaish Ehsan <awaishehsan077@gmail.com>
2026-03-28 21:32:25 +0530

    feat(validators): add change password, forgot password, and reset password validators

--- src/validators/index.js
+++ src/validators/index.js
@@ -51,7 +51,34 @@ const userLoginValidator = () => {
     ]
 }
 
+const userChangeCurrentPasswordValidator = ()=>{
+    return [
+        body("oldPassword").notEmpty().withMessage("Old password is required"),
+        body("newPassword").notEmpty().withMessage("New Password is required"),
+    ];
+
+}
+
+const userForgotPasswordValidator = ()=>{
+    return [
+        body("email")
+            .notEmpty()
+            .withMessage("Email is required")
+            .isEmail()
+            .withMessage("Email is invalid")
+    ]
+}
+
+const userResetForgotPasswordValidator = ()=>{
+    return [
+        body("newPassword").notEmpty().withMessage("Password is required")
+    ]
+}
+
 export {
     userRegisterValidator,
-    userLoginValidator
+    userLoginValidator,
+    userChangeCurrentPasswordValidator,
+    userForgotPasswordValidator,
+    userResetForgotPasswordValidator
 }
