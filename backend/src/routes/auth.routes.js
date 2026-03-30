8c425a2d81319f4fe58d4b2742e9f20bb1dfa955
Awaish Ehsan <awaishehsan077@gmail.com>
2026-03-30 19:11:49 +0530

    fix: remove duplicate resend route, make resend public

--- backend/src/routes/auth.routes.js
+++ backend/src/routes/auth.routes.js
@@ -6,20 +6,18 @@ import { verifyJWT } from "../middlewares/auth.middleware.js";
 
 const router = Router()
 
-//unsecured route
-router.route("/register").post(userRegisterValidator(),validate,registerUser)
-router.route("/login").post(userLoginValidator(),validate,login)
+// public routes
+router.route("/register").post(userRegisterValidator(), validate, registerUser)
+router.route("/login").post(userLoginValidator(), validate, login)
 router.route("/verify-email/:verificationToken").get(verifyEmail)
-router.route("/refresh-token").post(refreshAccessToken);
-router.route("/forgot-password").post(userForgotPasswordValidator(),validate,forgotPasswordRequest)
-router.route("/reset-password/:resetToken").post(userResetForgotPasswordValidator(),validate,resetForgotPassword)
-router.route("/resend-email-verification").post(verifyJWT, resendEmailVerification)
+router.route("/refresh-token").post(refreshAccessToken)
+router.route("/forgot-password").post(userForgotPasswordValidator(), validate, forgotPasswordRequest)
+router.route("/reset-password/:resetToken").post(userResetForgotPasswordValidator(), validate, resetForgotPassword)
+router.route("/resend-email-verification").post(resendEmailVerification)
 
-
-//secured route
-router.route('/logout').post(verifyJWT,logoutUser)
-router.route('/current-user').get(verifyJWT,getCurrentUser)
-router.route('/change-password').post(verifyJWT,userChangeCurrentPasswordValidator(),validate,changeCurrentPassword)
-
+// protected routes
+router.route('/logout').post(verifyJWT, logoutUser)
+router.route('/current-user').get(verifyJWT, getCurrentUser)
+router.route('/change-password').post(verifyJWT, userChangeCurrentPasswordValidator(), validate, changeCurrentPassword)
 
 export default router
